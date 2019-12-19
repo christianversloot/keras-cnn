8c17e721bfb1ea4dd1db9819f7704e8b3177bc94
Christian Versloot <mail@christianversloot.nl>
2019-12-19 08:24:18 +0100

    ELU & comparison script

---
+++ model_elu.py
@@ -0,0 +1,94 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.initializers import Constant
+from keras import backend as K
+from keras.layers import ELU
+import matplotlib.pyplot as plt
+
+# Model configuration
+img_width, img_height = 28, 28
+batch_size = 250
+no_epochs = 25
+no_classes = 10
+validation_split = 0.2
+verbosity = 1
+elu_alpha = 0.1
+
+# Load MNIST dataset
+(input_train, target_train), (input_test, target_test) = mnist.load_data()
+
+# Reshape data based on channels first / channels last strategy.
+# This is dependent on whether you use TF, Theano or CNTK as backend.
+# Source: https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
+if K.image_data_format() == 'channels_first':
+    input_train = input_train.reshape(input_train.shape[0], 1, img_width, img_height)
+    input_test = input_test.reshape(input_test.shape[0], 1, img_width, img_height)
+    input_shape = (1, img_width, img_height)
+else:
+    input_train = input_train.reshape(input_train.shape[0], img_width, img_height, 1)
+    input_test = input_test.reshape(input_test.shape[0], img_width, img_height, 1)
+    input_shape = (img_width, img_height, 1)
+
+# Parse numbers as floats
+input_train = input_train.astype('float32')
+input_test = input_test.astype('float32')
+
+# Normalize data [0, 1].
+input_train = input_train / 255
+input_test = input_test / 255
+
+# Convert target vectors to categorical targets
+target_train = keras.utils.to_categorical(target_train, no_classes)
+target_test = keras.utils.to_categorical(target_test, no_classes)
+
+# Create the model
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), input_shape=input_shape, kernel_initializer='he_normal'))
+model.add(ELU(alpha=elu_alpha))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, kernel_size=(3, 3), kernel_initializer='he_normal'))
+model.add(ELU(alpha=elu_alpha))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(256, kernel_initializer='he_normal'))
+model.add(ELU(alpha=elu_alpha))
+model.add(Dense(no_classes, activation='softmax', kernel_initializer='he_normal'))
+
+# Compile the model
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+# Fit data to model
+history = model.fit(input_train, target_train,
+          batch_size=batch_size,
+          epochs=no_epochs,
+          verbose=verbosity,
+          validation_split=validation_split)
+
+
+# Generate generalization metrics
+score = model.evaluate(input_test, target_test, verbose=0)
+print(f'Test loss for Keras ELU CNN: {score[0]} / Test accuracy: {score[1]}')
+
+# Visualize model history
+plt.plot(history.history['accuracy'], label='Training accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation accuracy')
+plt.title('ELU training / validation accuracies')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(loc="upper left")
+plt.show()
+
+plt.plot(history.history['loss'], label='Training loss')
+plt.plot(history.history['val_loss'], label='Validation loss')
+plt.title('ELU training / validation loss values')
+plt.ylabel('Loss value')
+plt.xlabel('Epoch')
+plt.legend(loc="upper left")
+plt.show()
